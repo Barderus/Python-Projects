64877ce957a922cd58c427927857db65d93f5f02
Gabriel dos Reis <dosreisg@dupage.edu>
2024-02-27 20:48:59 -0600

    First commit

---
+++ Car info/car_info_visual.py
@@ -0,0 +1,58 @@
+'''
+    Car Maintenance price 
+'''
+import matplotlib as plt
+
+title = "2015 Volkswagen Tiguan (Red)"
+
+'''
+12/31/2021 -> $1925.00 -> 103,981 miles
+02/28/2022 -> $238.72 -> 106,908
+05/23/2022 -> $1884.29 -> 111,447
+06/29/2022 -> $226.67 -> 112.816
+07/19/2022 -> $129.59 -> 113,639
+09/01/2022 -> $120.84 -> 115,386
+09/02/2022 -> $1338.00 -> 115,400
+09/16/2022 -> $257.80 -> 116.164
+11/09/2022 -> $37.78 -> 117,500
+11/28/2022 -> $1063.60 -> 119.299
+01/11/2023 -> $101.40 -> 120.279
+03/09/2023 -> $1709.66 -> 123.011
+06/27/2023 -> $1582.20 -> 128,619
+08/31/2023 -> $1003.75 -> 131,963
+10/24/2023 -> $101.40 -> 135,066
+12/19/2023 -> $1856.20 -> 137,356
+
+I want to know:
+    * How many miles driven total from 2021 to 2023
+    * How much it was spent on each year, the highest price, the lowest price
+    * Know how often a service was done to the car (?)
+    * Services by season (?)
+'''
+
+def menu():
+    pass
+
+def get_total_miles():
+    pass
+
+def get_total():
+    pass
+
+def get_highest():
+    pass
+
+def get_lowest():
+    pass
+
+def how_often():
+    pass
+
+def get_per_season():
+    pass
+
+def main():
+    pass
+
+if "__name__" == "___main__":
+    main()
