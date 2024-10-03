b47ecf35b3764acbff75dd6002c0eece989949b8
Gabriel dos Reis <dosreisg@dupage.edu>
2024-10-03 10:18:38 -0500

    Added functionality to the damage, cast_magic, and cast_heal methods

--- RPG Script/person.py
+++ RPG Script/person.py
@@ -1,4 +1,3 @@
-
 class Person:
     def __init__(self, name, health, mp, atk, df, mgk_atk, mgk_def, items, spells, descri):
         self.name = name
@@ -12,20 +11,25 @@ class Person:
         self.spells = spells
         self.descri = descri
 
-    def damage(self, atk):
-        pass
+    def damage(self, target):
+        dmg = self.atk - target.df
+        if dmg < 0:
+            dmg = 0
+        target.health -= dmg
+        print(f"{self.name} attacks {target.name} for {dmg} points of damage")
 
-    def cast_magic(self):
-        pass
+    def cast_magic(self, target, magic):
+        mp_cost = self.mp - magic.mp
+        dmg = self.mgk_atk - target.mgk_def
+        if dmg < 0:
+            dmg = 0
+        target.health -= dmg
+        print(f"{self.name} casts [spell name] on {target.name} dealing {dmg} points of damage")
 
-    def cast_heal(self):
-        pass
-
-    def take_damage(self, damage):
-        pass
-
-    def reduce_mp(self):
-        pass
+    def cast_heal(self, target, magic):
+        mp_cost = self.mp - magic.mp
+        target.health += magic.heal
+        print(f"{self.name} healed {target.name} for {magic.heal} health points")
 
     def choose_magic(self):
         pass
