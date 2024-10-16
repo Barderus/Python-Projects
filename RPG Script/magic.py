d569617214f17fde7442ae0c95e6390b2ccf6bfd
Gabriel dos Reis <dosreisg@dupage.edu>
2024-10-16 08:54:51 -0500

    Added damage type attribute to the magic class

--- RPG Script/magic.py
+++ RPG Script/magic.py
@@ -1,10 +1,11 @@
 class Magic:
-    def __init__(self, name, school, dmg, heal, mp, descri):
+    def __init__(self, name, school, dmg, heal, mp, dmg_type, descri):
         self.name = name
         self.mp = mp
         self.school = school
         self.dmg = dmg
         self.heal = heal
+        self.dmg_type = dmg_type
         self.descri = descri
 
     def __str__(self):
