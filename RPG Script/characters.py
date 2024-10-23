ed8bba03c2cc82f9859c156decf80ada7fe7a4fc
Gabriel dos Reis <dosreisg@dupage.edu>
2024-10-22 22:45:00 -0500

    Balanced the spell damage and mp necessary to cast the spells.

--- RPG Script/characters.py
+++ RPG Script/characters.py
@@ -1,33 +1,33 @@
 from person import Person
 from magic import Magic
 
-fire = Magic("Fire", "black", 600, 0, 25, "fire",
+fire = Magic("Fire", "black", 40, 0, 25, "fire",
              "The mage emits a jolt of fire from their hands towards the enemy")
-thunder = Magic("Thunder", "black", 600, 0, 25, "thunder",
+thunder = Magic("Thunder", "black", 40, 0, 25, "thunder",
                 "Lightning jolts from the mage's hand towards the enemy")
-blizzard = Magic("Blizzard", "black", 600, 0, 25, "ice",
+blizzard = Magic("Blizzard", "black", 40, 0, 25, "ice",
                  "A cold wind forms icicles that shoot towards the enemy")
-meteor = Magic("Meteor", "black", 1200, 0, 50, "fire",
+meteor = Magic("Meteor", "black", 100, 0, 50, "fire",
                "A small meteor shower crashes down upon the enemy")
-quake = Magic("Quake", "black", 140, 0, 15, "earth",
+quake = Magic("Quake", "black", 60, 0, 15, "earth",
               "The ground trembles as fissures open beneath the enemy's feet")
-tornado = Magic("Tornado", "black", 1600, 0, 45, "wind",
+tornado = Magic("Tornado", "black", 65, 0, 45, "wind",
                 "A fierce whirlwind surrounds the enemy, slashing at them with violent winds")
-ultima = Magic("Ultima", "black", 3000, 0, 100, "water",
+ultima = Magic("Ultima", "black", 110, 0, 100, "water",
                "The ultimate destructive spell, causing catastrophic damage to all enemies")
-dark = Magic("Dark", "black", 800, 0, 30, "dark",
+dark = Magic("Dark", "black", 60, 0, 30, "dark",
              "A shadowy force strikes the enemy, dealing damage and shrouding them in darkness.")
-bio = Magic("Bio", "black", 500, 0, 25, "wind",
+bio = Magic("Bio", "black", 50, 0, 25, "wind",
             "A poisonous wind that deals damage over time to the target.")
-drain = Magic("Drain", "black", 400, 200, 20, "health points",
+drain = Magic("Drain", "black", 40, 200, 20, "health points",
               "A sinister spell that drains the target's life force, restoring a portion of health to the caster.")
 
 # Creating White spells
-cure = Magic("Cure", "white", None, 100, 20, None,
+cure = Magic("Cure", "white", None, 50, 20, None,
              "A gentle green light envelops the target, healing their wounds")
-cura = Magic("Cura", "white", None, 200, 40, None,
+cura = Magic("Cura", "white", None, 75, 40, None,
              "A stronger healing light mends the target's deeper wounds")
-curaga = Magic("Curaga", "white", None, 400, 60, None,
+curaga = Magic("Curaga", "white", None, 100, 60, None,
                "A powerful healing force restores the target to full hp")
 revive = Magic("Revive", "white", None, 0, 50, None,
                "A soft light envelops the fallen ally, reviving them with 10% of their maximum HP")
@@ -41,58 +41,58 @@ speed = Magic("Speed", "green", None, 0, 30, None,
               "A spell that enhances the speed of all allies, allowing them to act more quickly in battle.")
 
 # Creating Blue Spell
-holy = Magic("Holy", "blue", 2000, 0, 80, "holy",
+holy = Magic("Holy", "blue", 80, 0, 80, "holy",
              "A blinding light descends from above, purging the enemy with divine power")
-flare = Magic("Flare", "black", 1800, 0, 75, "holy",
+flare = Magic("Flare", "black", 100, 0, 75, "holy",
               "A concentrated burst of descends from the sky, engulfing the enemy in holy flames")
 
 
 # Creating player avatar
 avatars= {
     "fighter": Person(
-        name="Aragorn", hp=900, mp=100, atk=200, df=70, speed = 70, mgk_atk=50, mgk_def=50,
+        name="Aragorn", hp=900, mp=60, atk=200, df=70, speed = 70, mgk_atk=50, mgk_def=50,
         items=[],
         spells=[],
         descri = "A fearless and stalwart protector, the Warrior excels in close combat and defense."
     ),
     "bruiser": Person(
-        name="Gimli", hp=1800, mp=100, atk=180, df=100, speed = 30, mgk_atk=30, mgk_def=60,
+        name="Gimli", hp=1800, mp=30, atk=180, df=100, speed = 30, mgk_atk=30, mgk_def=60,
         items=[],
         spells=[],
         descri = "A stout and resilient fighter, known for his unwavering strength and indomitable spirit"
     ),
     "barbarian": Person(
-        name="Conan", hp=950, mp=150, atk=220, df=50, speed = 60, mgk_atk=40, mgk_def=50,
+        name="Conan", hp=950, mp=30, atk=220, df=50, speed = 60, mgk_atk=40, mgk_def=50,
         items=[],
         spells=[],
         descri = "A fierce and untamed warrior, the Barbarian relies on raw power and savage attacks to overwhelm foes."
     ),
     "healer": Person(
-        name="Aerith", hp=700, mp=500, atk=30, df=80, speed = 45, mgk_atk=150, mgk_def=100,
+        name="Aerith", hp=700, mp=180, atk=30, df=80, speed = 45, mgk_atk=150, mgk_def=100,
         items=[],
         spells=[cura, curaga, revive, flare],
         descri = "A compassionate and wise protector, the Healer specializes in restoring hp and safeguarding allies."
     ),
     "green mage": Person(
-        name="Gandalf", hp=750, mp=600, atk=80, df=75, speed = 40, mgk_atk=200, mgk_def=90,
+        name="Gandalf", hp=750, mp=200, atk=80, df=75, speed = 40, mgk_atk=200, mgk_def=90,
         items=[],
         spells=[cure, protect, shell, holy],
         descri = "A versatile caster, the Green Mage blends healing magic with elemental attacks, balancing support and offense."
     ),
     "wizard": Person(
-        name="Snape", hp=680, mp=600, atk=40, df=70, speed = 50, mgk_atk=220, mgk_def=70,
+        name="Snape", hp=680, mp=220, atk=40, df=70, speed = 50, mgk_atk=220, mgk_def=70,
         items=[],
         spells=[thunder, ultima, quake, tornado],
         descri = "A master of destructive magic, the Black Mage wields powerful spells to annihilate enemies from afar."
     ),
     "sorcerer": Person(
-        name="Vivi", hp=650, mp=600, atk=40, df=70, speed = 60, mgk_atk=200, mgk_def=100,
+        name="Vivi", hp=650, mp=190, atk=40, df=70, speed = 60, mgk_atk=200, mgk_def=100,
         items=[],
         spells=[fire, blizzard, ultima, meteor],
         descri = "A dark and enigmatic sorcerer, this Black Mage conjures arcane forces to devastate foes with precision."
     ),
     "you": Person(
-        name= "You", hp=125, mp=500, atk=100, df=65, speed = 80, mgk_atk=125, mgk_def=80,
+        name= "You", hp=125, mp=100, atk=100, df=65, speed = 80, mgk_atk=125, mgk_def=80,
         items=[],
         spells=[],
         descri = "The destined champion, the Hero embodies the player's choices, leading the charge against evil with courage and valor."
@@ -102,49 +102,49 @@ avatars= {
 # Create the NPC dictionary
 allies = {
     "liora": Person(
-        name="Liora the Healer", hp=600, mp=600, atk=55, df=50, speed = 35, mgk_atk=180, mgk_def=100,
+        name="Liora the Healer", hp=600, mp=180, atk=55, df=50, speed = 35, mgk_atk=180, mgk_def=100,
         items=[],
         spells=[cure, cura, curaga, revive],
         descri="A compassionate healer from the White Lotus Order. Liora is known for her ability to mend even the most grievous wounds."
     ),
     "zarek": Person(
-        name="Zarek the Black Mage", hp=500, mp=650, atk=50, df=65, speed = 65, mgk_atk=220, mgk_def=75,
+        name="Zarek the Black Mage", hp=500, mp=200, atk=50, df=65, speed = 65, mgk_atk=220, mgk_def=75,
         items=[],
         spells=[fire, thunder, blizzard],
         descri="A powerful black mage with mastery over the elements. Zarek's spells bring destruction to his foes."
     ),
     "eldric": Person(
-        name="Eldric the Warrior", hp=650, mp=100, atk=120, df=70, speed = 90, mgk_atk=80, mgk_def=60,
+        name="Eldric the Warrior", hp=650, mp=50, atk=120, df=70, speed = 90, mgk_atk=80, mgk_def=60,
         items=[],
         spells=[],
         descri="A seasoned warrior known for his unshakable resolve and strength in battle. Eldric excels in close combat."
     ),
     "sylphra": Person(
-        name="Sylphra the White Mage", hp=450, mp=500, atk=40, df=50, speed = 50, mgk_atk=150, mgk_def=110,
+        name="Sylphra the White Mage", hp=450, mp=180, atk=40, df=50, speed = 50, mgk_atk=150, mgk_def=110,
         items=[],
         spells=[cura, protect, shell],
         descri="A gentle white mage with a deep connection to the divine. Sylphra uses her magic to protect and heal her allies."
     ),
     "thorn": Person(
-        name="Thorne the Dark Blade", hp=620, mp=245, atk=90, df=65, speed = 55, mgk_atk=100, mgk_def=55,
+        name="Thorne the Dark Blade", hp=620, mp=150, atk=90, df=65, speed = 55, mgk_atk=100, mgk_def=55,
         items=[],
         spells=[dark, drain],
         descri="A warrior with a mastery of dark magic, Thorne strikes fear into the hearts of his enemies with his cursed blade."
     ),
     "faye": Person(
-        name="Faye the Mystic", hp=430, mp=760, atk=50, df=50, speed = 75, mgk_atk=220, mgk_def=75,
+        name="Faye the Mystic", hp=430, mp=170, atk=50, df=50, speed = 75, mgk_atk=220, mgk_def=75,
         items=[],
         spells=[bio, meteor, speed],
         descri="A mysterious mage who wields unconventional magic. Faye uses forbidden spells to outsmart and debilitate her foes."
     ),
     "garrick": Person(
-        name="Garrick the Guardian", hp=800, mp=80, atk=120, df=100, speed = 40, mgk_atk=35, mgk_def=50,
+        name="Garrick the Guardian", hp=800, mp=40, atk=120, df=100, speed = 40, mgk_atk=35, mgk_def=50,
         items=[],
         spells=[],
         descri="A stalwart warrior who prioritizes defense. Garrick’s immense strength and resilience make him a formidable protector."
     ),
     "talia": Person(
-        name="Talia the Battle Cleric", hp=520, mp=350, atk=85, df=65, speed = 45, mgk_atk=90, mgk_def=85,
+        name="Talia the Battle Cleric", hp=520, mp=120, atk=85, df=65, speed = 45, mgk_atk=90, mgk_def=85,
         items=[],
         spells=[holy, flare, cure, protect],
         descri="A battle-hardened cleric who wields both a mace and holy magic. Talia can fight on the frontlines while supporting her allies."
