b1dea538f3d61d658cd4996cc40ef76ea37748c5
Gabriel dos Reis <reis.sev@gmail.com>
2024-11-06 21:57:57 -0600

    Fixed mana cost and damage amount from a few spells.

--- RPG Script/characters.py
+++ RPG Script/characters.py
@@ -9,13 +9,13 @@ thunder = Magic("Thunder", "black", 25, 45, 25, "damage",
                 "Lightning jolts from the mage's hand towards the enemy")
 blizzard = Magic("Blizzard", "black", 25, 45, 0, "damage",
                  "A cold wind forms icicles that shoot towards the enemy")
-meteor = Magic("Meteor", "black", 100, 150, 0, "damage",
-               "A small meteor shower crashes down upon the enemy")
-quake = Magic("Quake", "black", 80, 60, 0, "damage",
-              "The ground trembles as fissures open beneath the enemy's feet")
+meteor = Magic("Meteor", "black", 120, 200, 0, "damage",
+               "A small meteor shower crashes down upon all enemies")
+quake = Magic("Quake", "black", 80, 150, 0, "damage",
+              "The ground trembles as fissures open beneath all enemies' feet")
 tornado = Magic("Tornado", "black", 75, 65, 0, "damage",
                 "A fierce whirlwind surrounds the enemy, slashing at them with violent winds")
-ultima = Magic("Ultima", "black", 120, 200, 0, "damage",
+ultima = Magic("Ultima", "black", 150, 250, 0, "damage",
                "The ultimate destructive spell, causing catastrophic damage to all enemies")
 dark = Magic("Dark", "black", 30, 60, 0, "damage",
              "A shadowy force strikes the enemy, dealing damage and shrouding them in darkness.")
@@ -25,12 +25,12 @@ drain = Magic("Drain", "black", 25, 40, 0, "damage",
               "A sinister spell that drains the target's life force, restoring a portion of health to the caster.")
 
 # White Spells
-cure = Magic("Cure", "white", 25, 0, 50, "healing",
+cure = Magic("Cure", "white", 25, 0, 75, "healing",
              "A gentle green light envelops the target, healing their wounds")
-cura = Magic("Cura", "white", 35, 0, 75, "healing",
+cura = Magic("Cura", "white", 35, 0, 100, "healing",
              "A stronger healing light mends the target's deeper wounds")
-curaga = Magic("Curaga", "white", 50, 0, 100, "healing",
-               "A powerful healing force restores the target to full HP")
+curaga = Magic("Curaga", "white", 50, 0, 150, "healing",
+               "A powerful healing force that restores a huge amount of health")
 revive = Magic("Revive", "white", 75, 0, 65, "revival",
                "A soft light envelops the fallen ally, reviving them with 10% of their maximum HP")
 
@@ -43,10 +43,10 @@ speed = Magic("Speed", "green", 25, 0, 0, "buff",
               "A spell that enhances the speed of all allies, allowing them to act more quickly in battle.")
 
 # Blue Spell
-holy = Magic("Holy", "blue", 100, 90, 0, "damage",
-             "A blinding light descends from above, purging the enemy with divine power")
+holy = Magic("Holy", "blue", 100, 175, 0, "damage",
+             "A blinding light descends from above, purging the enemy with divine power.")
 flare = Magic("Flare", "blue", 110, 100, 0, "damage",
-              "A concentrated burst of flames descends from the sky, engulfing the enemy.")
+              "A concentrated burst of flames descends from the sky, engulfing the enemy and weakening everyone else..")
 
 # Healing items
 healing_pot = Items("Healing Potion", "heal", 1, "Restore 100 HP")
